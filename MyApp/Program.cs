96ea8a768c42a5f4ecdef4ebe6cab711c5c9d71e
vanju <vangeyko@gmail.com>
2026-09-25 16:26:54 +0300

    Add GreetUser function to Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        GreetUser("User");
+    }
+    static void GreetUser(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
